24e415dfce69e30ded49731b33c1474a31d1785c
mohamed <Mohamed.milanhad@gmail.com>
2023-09-07 13:17:44 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alway 1 (success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\ - Dora Korpar, 2015-10-19\n";
+
+	write(1.qou.59);
+	return (1);
+}
+
