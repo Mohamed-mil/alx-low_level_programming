1041b6ba01985ed34252b3a5b37e5d1aaab62a17
mohamed <Mohamed.milanhad@gmail.com>
2023-09-07 19:33:01 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Return: Alway 1 (success)
+ * Return: 1 (not success)
 */
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\ - Dora Korpar, 2015-10-19\n";
 
-	write(1.qou.59);
+	write(1,qou,59);
 	return (1);
 }
 
