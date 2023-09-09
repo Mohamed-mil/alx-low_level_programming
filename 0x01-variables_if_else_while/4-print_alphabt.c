2345f46e4d197ea78c19d98ac02b0f26b7176067
mohamed <Mohamed.milanhad@gmail.com>
2023-09-08 17:13:23 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char h = 'a';
+
+	while (h <= 'z')
+	{
+		if (h == 'e' || h == 'q')
+			h++;
+		putchar(h);
+		h++
+	}
+	putchar('\n');
+	return (0);
+}
