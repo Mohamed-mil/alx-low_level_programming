0120e725bfd3007c72fc4b72c51d63dfeba7bbfe
mohamed <Mohamed.milanhad@gmail.com>
2023-09-08 19:14:50 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int d = 48;
+
+	while (d <= 102)
+	{
+		putchar(d);
+
+		if (d == 57)
+			d += 39 :
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
