38e56898a56e408c88432be64a8fb1abfa2a16a1
mohamed <Mohamed.milanhad@gmail.com>
2023-09-12 10:26:58 -0700

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,13 @@
 /**
  * _islower - function to check if the cha is lowercase
  *
- * Return: 1 if 'c' is lowercase
+ * @x: checks input of function
+ * Return: 1 if 'x' is lowercase
+ *         otherwise always 0 (success)
 */
-
-int _islower(int x)
+int _islower(int x);
 {
-        if (x >= 97 && x <= 122)
-                return (1);
-        return (0);
+	if (x >= 97 && x <= 122)
+		return (1);
+	return (0);
 }
