cb7dcae758eabb8b1bdddd764b85fee86fadaf04
mohamed <Mohamed.milanhad@gmail.com>
2023-09-12 08:41:59 -0700

    a

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - function to check if the cha is lowercase
+ *
+ * Return: 1 if 'c' is lowercase
+*/
+
+int _islower(int x)
+{
+	if (x >= 97 && x <= 122)
+		return (1);
+	return (0);
+}
