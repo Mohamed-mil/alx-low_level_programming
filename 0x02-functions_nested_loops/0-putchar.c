b96d43d30980257516a63ba54ec863cb2442d92d
mohamed <Mohamed.milanhad@gmail.com>
2023-09-12 07:06:45 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * main - entry point
+ *
+ * Return (0) (success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+		_putchar(str[x]);
+	_putchar('\n')
+
+		return (0);
+}
