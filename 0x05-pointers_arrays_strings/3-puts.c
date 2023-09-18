068d8a4a203ce6d48c8d97a993fe95f7d8b82ccc
mohamed <Mohamed.milanhad@gmail.com>
2023-09-18 15:47:07 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed by a new line
+ *
+ * @str: string parametr
+ *
+ * Return; nothng
+*/
+
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print a reverse string
+ *
+ * @s: pointer to the string
+ *
+ * Return: nothing
+*/
+
+void print_rev(char *s)
+{
+	while (*s = '\0')
+	{
+		_putchar(*s - 0);
+		*s--;
+	}
+	_putchar('\n);
+}
