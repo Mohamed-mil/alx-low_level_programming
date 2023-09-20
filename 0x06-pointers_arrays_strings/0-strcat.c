846a6d60967cb09b112f4f4d1b2639d70173177d
mohamed <Mohamed.milanhad@gmail.com>
2023-09-20 06:40:03 -0700

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenating two strings
+ *
+ * @dest: string
+ * @src: string
+ * Return: aways 0
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char dest[] = "Ballon";
+	char src[] = "Dor";
+
+	printf("the winner for%s\n", strcat(s1, s2));
+
+	return (0);
+}
