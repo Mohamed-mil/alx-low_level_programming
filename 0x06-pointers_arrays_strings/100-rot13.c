3996fbc850c930db5a7236e35b9d3e2c2a8f1155
mohamed <Mohamed.milanhad@gmail.com>
2023-09-21 14:48:46 -0700

    a

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *rot13 - rotate the alphapet by 13 place
+ * @n: string
+ *
+ * Return: s
+*/
+
+char *rot13(char *n)
+{
+	int i, j;
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotdata[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data[j])
+			{
+				s[i] = rotdata[j];
+				break;
+			}
+		}
+
+	}
+	return (s);
+}
