185ab9135011c25c9c7fccb9878974a5020d6f1d
mohamed <Mohamed.milanhad@gmail.com>
2023-09-22 06:49:46 -0700

    a

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,13 +11,12 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	i = 0;
-
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str <= 'z')
+		if (str[i] >= '97' && str[i] <= '122')
+		{
 			str[i] = str[i] - 32;
-			i++;
+		}
 	}
 	return (str);
 }
