5a93945773fbf0e224f4fb4ad779951f37b92830
mohamed <Mohamed.milanhad@gmail.com>
2023-09-25 07:30:26 -0700

    a

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print the sum
+ *
+ * @a: 2d array
+ * @size: size of array
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, s = 0, s1 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s += a[i];
+		s1 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", s);
+	print("%d\n", s1);
+}
