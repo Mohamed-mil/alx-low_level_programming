63dd8ae6e83cfa6728ec49a70cefc8d066247a13
mohamed <Mohamed.milanhad@gmail.com>
2023-10-02 13:25:26 -0700

    a

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of 2 numbers
+ * @argc: number of commandline argumnts
+ * @argv: pointer to ana array
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+	return (0);
+}
