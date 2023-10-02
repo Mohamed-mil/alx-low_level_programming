b9b0b84ac4f65deb568d74d4ada325e461ba7a8b
mohamed <Mohamed.milanhad@gmail.com>
2023-10-02 12:17:52 -0700

    a

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,5 +1,27 @@
 #include "main.h"
 
+
+/**
+ * _strncpy - copies two striing
+ * @dest: string
+ * @src: string
+ * @n: bytes of src
+ * Return: @dest
+*/
+
+
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
