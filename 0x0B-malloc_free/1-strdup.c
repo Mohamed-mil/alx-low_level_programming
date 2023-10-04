a7f448389d2751c2c08d40f8ded56f6a3ac597f3
mohamed <Mohamed.milanhad@gmail.com>
2023-10-04 06:15:42 -0700

    a

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,12 +14,15 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	for (; str[size] != '\0'; size++);
+
+	for ( ; str[size] != '\0'; size++)
+		;
 
 	n = malloc(size * sizeof(*str) + 1);
 
 	if (n == 0)
 		return (NULL);
+
 	else
 	{
 		for (; i < size; i++)
